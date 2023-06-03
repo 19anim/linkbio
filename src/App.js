775afcb5abde0435ef6d3a4b1e6da943e319832c
19.anim <nptatdt@gmail.com>
2023-06-04 05:19:39 +0700

    editting css style

--- src/App.js
+++ src/App.js
@@ -3,69 +3,91 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="linkbio__title">
-        Ở ĐÂY IEM BÁN THUỐC HO CON HƯƠU XỐP
-      </div>
+      <div className="linkbio__title">Ở ĐÂY IEM BÁN THUỐC HO CON HƯƠU XỐP</div>
 
       {/* INSTAGRAM LINK */}
-      <div className="linkbio__items border">
-          <span className="linkbio__items-icon image__ig"></span>
-          <a
-            className="linkbio__items-link"
-            href="https://www.instagram.com/jagerthejager"
-            target="_blank"
-          >
+      <div className="linkbio__items-container">
+        <a
+          className="linkbio__items-link"
+          href="https://www.instagram.com/jagerthejager"
+          target="_blank"
+        >
+          <div className="linkbio__items border">
+            <span className="linkbio__items-icon image__ig"></span>
             <p>Instagram xốp nè</p>
-          </a>
+          </div>
+        </a>
       </div>
 
       {/* FACEBOOK LINK */}
-      <div className="linkbio__items border">
-      <span className="linkbio__items-icon image__fb"></span>
+      <div className="linkbio__items-container">
         <a
           className="linkbio__items-link"
           href="https://www.facebook.com/profile.php?id=100092571115747"
           target="_blank"
         >
-          <p>Facebook chính của xốp</p>
+          <div className="linkbio__items border">
+            <span className="linkbio__items-icon image__fb"></span>
+            <p>Page Facebook chính của xốp</p>
+          </div>
         </a>
       </div>
 
       {/* FACEBOOK LINK */}
-      <div className="linkbio__items border">
-      <span className="linkbio__items-icon image__fb"></span>
+      <div className="linkbio__items-container">
         <a
           className="linkbio__items-link"
           href="https://www.facebook.com/jagerthejagerxop"
           target="_blank"
         >
-          <p>Facebook phụ của xốp</p>
+          <div className="linkbio__items border">
+            <span className="linkbio__items-icon image__fb"></span>
+            <p>Page Facebook phụ của xốp</p>
+          </div>
+        </a>
+      </div>
+
+      {/* FACEBOOK LINK */}
+      <div className="linkbio__items-container">
+        <a
+          className="linkbio__items-link"
+          href="https://www.facebook.com/19anim"
+          target="_blank"
+        >
+          <div className="linkbio__items border">
+            <span className="linkbio__items-icon image__fb"></span>
+            <p>Facebook cá nhân của xốp</p>
+          </div>
         </a>
       </div>
 
       {/* ZALO LINK */}
-      <div className="linkbio__items border">
-      <span className="linkbio__items-icon image__zalo"></span>
+      <div className="linkbio__items-container">
         <a
           className="linkbio__items-link"
           href="https://zalo.me/0927183879"
           target="_blank"
         >
-          <p>Zalo của xốp - 0927183879</p>
+          <div className="linkbio__items border">
+            <span className="linkbio__items-icon image__zalo"></span>
+            <p>Zalo của xốp - 0927183879</p>
+          </div>
         </a>
       </div>
 
       {/* SHOPEE LINK */}
-      <div className="linkbio__items border">
-      <span className="linkbio__items-icon image__shopee"></span>
+      <div className="linkbio__items-container">
         <a
           className="linkbio__items-link"
           href="https://shopee.vn/jagerthejager"
           target="_blank"
         >
-          <p>Shopee của xốp</p>
+          <div className="linkbio__items border">
+            <span className="linkbio__items-icon image__shopee"></span>
+            <p>Shopee của xốp</p>
+          </div>
         </a>
-      </div>
+      </div>  
     </div>
   );
 }
