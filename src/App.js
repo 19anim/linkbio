dbb5fcc47b92fa6ac66b468a0ae4e06987c0bbc8
19.anim <nptatdt@gmail.com>
2023-06-04 04:34:45 +0700

    Complete the linkbio project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="linkbio__title">
+        Ở ĐÂY IEM BÁN THUỐC HO CON HƯƠU XỐP
+      </div>
+
+      {/* INSTAGRAM LINK */}
+      <div className="linkbio__items border">
+          <span className="linkbio__items-icon image__ig"></span>
+          <a
+            className="linkbio__items-link"
+            href="https://www.instagram.com/jagerthejager"
+            target="_blank"
+          >
+            <p>Instagram xốp nè</p>
+          </a>
+      </div>
+
+      {/* FACEBOOK LINK */}
+      <div className="linkbio__items border">
+      <span className="linkbio__items-icon image__fb"></span>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="linkbio__items-link"
+          href="https://www.instagram.com/jagerthejager"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          <p>Facebook chính của xốp</p>
         </a>
-      </header>
+      </div>
+
+      {/* FACEBOOK LINK */}
+      <div className="linkbio__items border">
+      <span className="linkbio__items-icon image__fb"></span>
+        <a
+          className="linkbio__items-link"
+          href="https://www.instagram.com/jagerthejager"
+          target="_blank"
+        >
+          <p>Facebook phụ của xốp</p>
+        </a>
+      </div>
+
+      {/* ZALO LINK */}
+      <div className="linkbio__items border">
+      <span className="linkbio__items-icon image__zalo"></span>
+        <a
+          className="linkbio__items-link"
+          href="https://www.instagram.com/jagerthejager"
+          target="_blank"
+        >
+          <p>Zalo của xốp</p>
+        </a>
+      </div>
+
+      {/* SHOPEE LINK */}
+      <div className="linkbio__items border">
+      <span className="linkbio__items-icon image__shopee"></span>
+        <a
+          className="linkbio__items-link"
+          href="https://www.instagram.com/jagerthejager"
+          target="_blank"
+        >
+          <p>Shopee của xốp</p>
+        </a>
+      </div>
     </div>
   );
 }
